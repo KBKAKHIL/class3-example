db33a6cfbc659a421b4db0554643e2cb73c7efc6
kbk <macstudent@10.10.150.106>
2019-09-05 11:42:14 -0400

    added some code

--- src/helloworld/Main.java
+++ src/helloworld/Main.java
@@ -13,6 +13,13 @@ public class Main {
 		String personname = keyboard.nextLine();
 		System.out.println("Hello " + personname);
 		
+		System.out.println("what is your age?");
+		int age = keyboard.nextInt();
+		int futureage = age + 10;
+		System.out.println("Right now your age is " + age);
+		System.out.println("and in 10 years you will be : " + futureage);
+		
+		
 	}
 
 }
